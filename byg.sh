90a242e3c70fe8d49b6d8df85cbd2aceb2ff48e8
Nikolaj Jehøj-Krogager <kagenogmig@gmail.com>
2026-07-04 18:45:18 +0200

    feat: update image handling and optimization scripts for JXL formats

--- byg.sh
+++ byg.sh
@@ -8,6 +8,9 @@ cd $PROJECT_DIR || exit
 # 2. Hent seneste ændringer fra git
 git pull origin main
 
+# 3. Optimer billeder (generer JXL)
+./optimize_images.sh
+
 /usr/bin/podman run --rm \
     -v "$PROJECT_DIR":/project:Z \
     --userns=keep-id \
